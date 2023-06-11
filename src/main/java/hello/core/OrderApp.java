e07e938ecd5555eb5c90537ac4eb072dd8a0627f
gorapaduckoo <jioung93@naver.com>
2023-06-12 01:34:23 +0900

    refactor: 생성자 주입으로 변경

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -11,8 +11,12 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+//        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = new OrderServiceImpl();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -21,6 +25,6 @@ public class OrderApp {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
         System.out.println("order = " + order);
-        System.out.println("order.calculatePrice = " + order.calculatePrice());
+//        System.out.println("order.calculatePrice = " + order.calculatePrice());
     }
 }
